e627cb66a12aee66070647c8cfd3bada0ccd87a8
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2022-07-28 15:43:32 +0630

    Commited first.

---
+++ getting_started/HelloJava.java
@@ -0,0 +1,7 @@
+package getting_started;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Greeting from Java!");
+    }    
+}
---
+++ getting_started/YourInitials.java
@@ -0,0 +1,15 @@
+package getting_started;
+
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("  SSS     A   ");
+        System.out.println(" S   S   A A ");
+        System.out.println(" S      A   A ");
+        System.out.println("  SSS   AAAAA ");
+        System.out.println("     S  A   A ");
+        System.out.println(" S   S  A   A ");
+        System.out.println("  SSS   A   A ");
+        System.out.println("");
+    }
+}
