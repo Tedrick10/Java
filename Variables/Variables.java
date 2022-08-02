df541288b57ee83b3f20e7de288ee4facb3c68e2
Thet Tun Kyaw <thettunkyaw301098@gmail.com>
2022-08-02 17:29:38 +0630

    Created Variables folder & Developed new java class concerned with variables such as int, long, float, double, and String.

---
+++ Variables/Variables.java
@@ -0,0 +1,8 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        int passengers = 20;
+        System.out.println("Passengers: " + passengers);
+    }    
+}
